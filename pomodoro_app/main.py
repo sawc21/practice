23df8a0e06c9a2201df2c36b0a671f0c00388ff4
Sawyer <scawthon@angelo.edu>
2026-02-19 15:54:34 -0600

    added pomodoro functions like countdowm timer

--- pomodoro_app/main.py
+++ pomodoro_app/main.py
@@ -22,6 +22,14 @@ def start_timer(n):
     print(n)
     if n> 0:
         window.after(1000,start_timer, n - 1)
+        
+def start():
+    start_timer(30)
+    
+def reset():
+    canvas.itemconfig(timer_text, text = timer)
+    window.after(1000,reset,timer)
+   
     
 window = Tk()
 window.title("Pomodoro")
@@ -30,19 +38,19 @@ window.config(padx=100,pady=50,bg=PINK)
 
 canvas= Canvas(width=200,height=224,bg=PINK,highlightthickness=0)
 tomato_img= PhotoImage(file="tomato.png")
-
+timer = 30
 canvas.create_image(100,112,image=tomato_img)
-timer_text=canvas.create_text(100,130,text='',fill="White",font=(FONT_NAME,35,"bold"))
+timer_text=canvas.create_text(100,130,text=timer,fill="White",font=(FONT_NAME,35,"bold"))
 canvas.grid(column=1,row=1)
-timer = start_timer(30)
+
 
 
 # timer= Label(text="Timer", font=(FONT_NAME,35,"bold"),bg= PINK, fg=GREEN , highlightthickness=0)
 # timer.grid(column=  1,row=0)
 
-start= Button(text="Start", command=start_timer)
+start= Button(text="Start", command=start)
 start.grid(column=0,row=3)
-Reset= Button(text="Reset", command='')
+Reset= Button(text="Reset", command=reset)
 Reset.grid(column=2,row=3)
 
 text = Label(text="✔",fg=GREEN,bg=PINK)
